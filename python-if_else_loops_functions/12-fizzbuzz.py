2c9d040d85b273e67dc30db1c3505ddfc2454a96
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-02 04:50:03 -0800

    new file

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for a in range(1, 101):
+        if a % 5 == 0 and a % 3 == 0:
+            print("FizzBuzz", end=" ")
+            continue
+        elif a % 5 == 0:
+            print("Buzz", end=" ")
+        elif a % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        print(a, end=" ")
