f02ed56d7d9c14faf59a565ea98711a075fd42d9
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-04 03:25:01 -0800

    new file

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -8,13 +8,13 @@ if __name__ == "__main__":
         op = argv[2]
         num1 = int(argv[1])
         num2 = int(argv[3])
-        if op is '+':
+        if op == '+':
             print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-        elif op is '-':
+        elif op == '-':
             print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
-        elif op is '*':
+        elif op == '*':
             print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-        elif op is '/':
+        elif op == '/':
             print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
