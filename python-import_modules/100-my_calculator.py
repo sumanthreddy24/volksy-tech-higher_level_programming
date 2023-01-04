39ffced72ff1dc875631d3f64a740b327bcf80fb
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-04 04:14:31 -0800

    new file

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, div, mul
     from sys import argv
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("./100-my_calculator.py <a> <operator> <b> ")
         exit(1)
-        op = argv[2]
-        num1 = int(argv[1])
-        num2 = int(argv[3])
-        if op == '+':
-            print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
-        elif op == '-':
-            print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
-        elif op == '*':
-            print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
-        elif op == '/':
-            print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    operator = argv[2]
+    n1 = int(argv[1])
+    n2 = int(argv[3])
+    if operator == "+":
+        print("{} + {} = {}".format(n1, n2, add(n1, n2)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(n1, n2, sub(n1, n2)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(n1, n2, mul(n1, n2)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(n1, n2, div(n1, n2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
