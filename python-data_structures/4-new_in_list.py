3e1dff420d7275fef94848db04f28bd5670ecd7b
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-05 03:14:24 -0800

    new file

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    for idx >= len(my_list) or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return my_list
     return my_list
 my_list[idx] = element
