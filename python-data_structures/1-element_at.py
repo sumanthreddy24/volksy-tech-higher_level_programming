e5148f3e834872623c670d92235d12265560b9f3
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-04 23:04:53 -0800

    new file

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,6 +2,4 @@
 def element_at(my_list, idx):
     if idx > len(my_list) or idx < 0:
         return none
-    else:
-        return ("Element at index {:d} is {}".format(idx, element_at(my_list, idx))
     return my_list[idx]
