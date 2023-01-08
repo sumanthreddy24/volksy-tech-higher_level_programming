efecfc0de419aab338a057b168dbb140d6921eeb
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-08 03:36:12 -0800

    new file

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,8 +2,9 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_value = list(a_dictionary.keys())[0]
-        for x in a_dictionary[x] > a_dictionary[ma_value]:
-            max_value = x
+        for x in a_dictionary.keys():
+            if a_dictionary[x] > a_dictionary[max_value]:
+                max_value = x
         return max_value
     else:
         return None
