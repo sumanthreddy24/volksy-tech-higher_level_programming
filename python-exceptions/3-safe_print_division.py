ebc968f5ea2f81ea28b992a330d1f632aca5f816
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-09 20:09:46 -0800

    ne file

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,4 +7,4 @@ def safe_print_division(a, b):
         result = None
         return result
     finally:
-        print("Inside rsult: {}".format(result))
+        print("Inside result: {}".format(result))
