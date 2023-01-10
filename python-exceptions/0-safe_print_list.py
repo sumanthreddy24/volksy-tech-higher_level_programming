04d4ba50512202a8808753d0530800459df59320
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-09 19:51:51 -0800

    new file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pthon3
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed = 0
+        while printed < x:
+            print(my_list[printed], end='')
+            printed += 1
+        print()
+    except:
+        print()
+    return printedi
