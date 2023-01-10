356d357108b448b0567ba723440ee557f3030e17
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-09 19:56:18 -0800

    new fiel

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        printed = 0
-        while printed < x:
-            print(my_list[printed], end='')
-            printed += 1
-        print()
-    except:
-        print()
-    return printedi
+    y = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[y]), end="")
+            y += 1
+        except:
+            pass
+        print('')
+        return y
