87fda121132c359223169742abcfb5f3193823cb
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-11 20:35:54 -0800

    new file

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,10 @@
 
 class Square:
     """square validation"""
-
-    def __init__(self, size=0)
-    """constructor"""
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self .__size = size
+    def __init__(self, size=0):
+        """constructor"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self .__size = size
