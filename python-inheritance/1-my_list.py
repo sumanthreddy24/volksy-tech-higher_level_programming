239c07e4ebfe4bb33b9f3431e88181053e1ec8f7
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-18 23:04:06 -0800

    d

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""defines class BaseGeomtry with public instance """
+"""interhitance"""
 
 
-class BaseGeometry:
-    """class with public instance method to raise exception"""
-
-    def area(self):
-        """raises exception that area method is not implemented"""
-
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        """validates integer"""
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+class MyList(list):
+    """creater te inheritance"""
+    def print_sorted(self):
+        """sorted"""
+        print(sorted(self))
