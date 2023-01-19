648b9540fc0c8ed361f0179a8d6dcd0300b03a83
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-18 20:21:16 -0800

    new file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Reading file """
+
+
+def read_file(filename=""):
+    """Defining function"""
+    if filename:
+        with open(filename, mode='r', encoding="utf-8") as f:
+            for i in f:
+                print(i, end="")
