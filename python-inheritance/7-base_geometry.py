cb0ff18693b4a195de0a3b454f9130de1001ea3d
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-19 00:47:23 -0800

    new file

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,17 +4,12 @@
 
 class BaseGeometry:
     """Contains functions area(), integer_validator()"""
-
     def area(self):
         """Function not implemented yet"""
-
         raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
         """Function that validates `value'"""
-
         if type(value) != int:
             raise TypeError(name + " must be an integer")
-
         if value <= 0:
             raise ValueError(name + " must be greater than 0")
