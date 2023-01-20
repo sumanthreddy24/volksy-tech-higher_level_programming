9c08bb996e72c24b076f3f20782c958ad5b07136
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-19 20:43:13 -0800

    new file

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""Module that returns the dictionary description
+with simple data structure for a JSON
+serialization of an object."""
+
+
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure
-        (list, dictionary, string, integer and boolean) for JSON serialization
-        of an object
-    Args:
-        obj(obj): object
-    Returns:
-        dictionary description for JSON serialization of an object
-    """
-    return obj.__dict__
+    """ return the dictionary description of an object"""
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
