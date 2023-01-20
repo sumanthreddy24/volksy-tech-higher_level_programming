41366b08b9dda39a027a9eafa97de8b0e71b33ac
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-19 22:53:21 -0800

    new file

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,7 +5,7 @@
 class Student:
     """Represent a student."""
 
-    def _init_(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -15,7 +15,7 @@ class Student:
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self._dict_
+        return self.__dict__
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student."""
