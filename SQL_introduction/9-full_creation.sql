d7f2b1818b2948f1d2338b62026796da698ef49f
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-01-30 01:58:07 -0800

    new file

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,9 @@
 -- tenth sql
-CREATE TABLE second_table (id int, name varchar(256), score int);
+CREATE TABLE second_table (
+	id int,
+       	name varchar(256),
+       	score int
+);
 INSERT INTO second_table (id, name, score) ('1', 'John', '10');
 INSERT INTO second_table (id, name, score) ('2', 'Alex', '3');
 INSERT INTO second_table (id, name, score) ('3', 'Bob', '14');
