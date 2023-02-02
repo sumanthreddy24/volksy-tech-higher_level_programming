3ed9d71837ef44b8d9f391f198a2462afdca2826
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-02-02 04:07:09 -0800

    new fiel

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,4 @@ LEFT OUTER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NOT NULL
 GROUP BY genre
-ORDER BY number_shows DESC;
+ORDER BY number_0f_shows DESC;
