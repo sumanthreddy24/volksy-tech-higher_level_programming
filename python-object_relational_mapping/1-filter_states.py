51b9a4ad524fc5093c270ff13805a245d04c993d
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-02-07 23:11:22 -0800

    new file

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states where name like 'N%'")
+    cur.execute("SELECT id, name FROM states where name like BINARY 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
