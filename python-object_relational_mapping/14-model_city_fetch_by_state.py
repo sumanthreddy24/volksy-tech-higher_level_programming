6f34725f34a323e4c077092535be31032c5ffb4b
sumanthreddy24 <sumanthtbrightcomgroup.com>
2023-02-12 20:16:44 -0800

    new file

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-'''
-Class definition of a City
-'''
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+"""Displays all City obj from db"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
 
 
-Base = declarative_base()
+def list_city_obj():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    rows = session.query(State, City).join(City).all()
+
+    for i in rows:
+        print("{}: ({}) {}".format(i[0].__dict__['name'],
+                                   i[1].__dict__['id'],
+                                   i[1].__dict__['name']))
+
+    session.close()
 
 
-class City(Base):
-    '''
-    City class inherits from Base
-    '''
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)3
+if __name__ == "__main__":
+    list_city_obj()
